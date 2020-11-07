b218e408a74085a0df4717e7c6a7f61ce7655f1f
Pavel Zyrianov <pastpav@gmail.com>
2020-11-07 15:49:33 +0200

    Add files via upload

---
+++ Tic_Tac_Toe_CSHARP_FORMS/Tic_Tac_Toe_CSHARP_FORMS/MainForm.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Tic_Tac_Toe_CSHARP_FORMS
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.playerNameX_textBox = new System.Windows.Forms.TextBox();
+            this.playerXLabel = new System.Windows.Forms.Label();
+            this.playerOLabel = new System.Windows.Forms.Label();
+            this.playerNameO_textBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.nextMoveInfoLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openLastGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resultXLabel = new System.Windows.Forms.Label();
+            this.resultOLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(13, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 133);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // playerNameX_textBox
+            // 
+            this.playerNameX_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerNameX_textBox.Location = new System.Drawing.Point(105, 466);
+            this.playerNameX_textBox.Name = "playerNameX_textBox";
+            this.playerNameX_textBox.Size = new System.Drawing.Size(147, 31);
+            this.playerNameX_textBox.TabIndex = 9;
+            // 
+            // playerXLabel
+            // 
+            this.playerXLabel.AutoSize = true;
+            this.playerXLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerXLabel.Location = new System.Drawing.Point(10, 470);
+            this.playerXLabel.Name = "playerXLabel";
+            this.playerXLabel.Size = new System.Drawing.Size(89, 24);
+            this.playerXLabel.TabIndex = 10;
+            this.playerXLabel.Text = "Player \'X\'";
+            // 
+            // playerOLabel
+            // 
+            this.playerOLabel.AutoSize = true;
+            this.playerOLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerOLabel.Location = new System.Drawing.Point(10, 507);
+            this.playerOLabel.Name = "playerOLabel";
+            this.playerOLabel.Size = new System.Drawing.Size(90, 24);
+            this.playerOLabel.TabIndex = 12;
+            this.playerOLabel.Text = "Player \'O\'";
+            // 
+            // playerNameO_textBox
+            // 
+            this.playerNameO_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerNameO_textBox.Location = new System.Drawing.Point(105, 503);
+            this.playerNameO_textBox.Name = "playerNameO_textBox";
+            this.playerNameO_textBox.Size = new System.Drawing.Size(147, 31);
+            this.playerNameO_textBox.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(152, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 133);
+            this.button2.TabIndex = 15;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(291, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 133);
+            this.button3.TabIndex = 16;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(13, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 133);
+            this.button4.TabIndex = 19;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(152, 176);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 133);
+            this.button5.TabIndex = 18;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(291, 176);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(133, 133);
+            this.button6.TabIndex = 17;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(12, 315);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(133, 133);
+            this.button7.TabIndex = 22;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(152, 315);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(133, 133);
+            this.button8.TabIndex = 21;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(291, 315);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(133, 133);
+            this.button9.TabIndex = 20;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // nextMoveInfoLabel
+            // 
+            this.nextMoveInfoLabel.AutoSize = true;
+            this.nextMoveInfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextMoveInfoLabel.Location = new System.Drawing.Point(58, 552);
+            this.nextMoveInfoLabel.Name = "nextMoveInfoLabel";
+            this.nextMoveInfoLabel.Size = new System.Drawing.Size(317, 55);
+            this.nextMoveInfoLabel.TabIndex = 23;
+            this.nextMoveInfoLabel.Text = "Next Move \'X\'";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(440, 24);
+            this.menuStrip1.TabIndex = 26;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openLastGameToolStripMenuItem,
+            this.saveGameToolStripMenuItem,
+            this.restartGameToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openLastGameToolStripMenuItem
+            // 
+            this.openLastGameToolStripMenuItem.Name = "openLastGameToolStripMenuItem";
+            this.openLastGameToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.openLastGameToolStripMenuItem.Text = "Open Game";
+            this.openLastGameToolStripMenuItem.Click += new System.EventHandler(this.openGameToolStripMenuItem_Click);
+            // 
+            // saveGameToolStripMenuItem
+            // 
+            this.saveGameToolStripMenuItem.Name = "saveGameToolStripMenuItem";
+            this.saveGameToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.saveGameToolStripMenuItem.Text = "Save Game";
+            this.saveGameToolStripMenuItem.Click += new System.EventHandler(this.saveGameToolStripMenuItem_Click);
+            // 
+            // restartGameToolStripMenuItem
+            // 
+            this.restartGameToolStripMenuItem.Name = "restartGameToolStripMenuItem";
+            this.restartGameToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.restartGameToolStripMenuItem.Text = "Restart Game";
+            this.restartGameToolStripMenuItem.Click += new System.EventHandler(this.restartGameToolStripMenuItem_Click);
+            // 
+            // resultXLabel
+            // 
+            this.resultXLabel.AutoSize = true;
+            this.resultXLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultXLabel.Location = new System.Drawing.Point(258, 470);
+            this.resultXLabel.Name = "resultXLabel";
+            this.resultXLabel.Size = new System.Drawing.Size(149, 24);
+            this.resultXLabel.TabIndex = 27;
+            this.resultXLabel.Text = "W-00; L-00; D-00";
+            // 
+            // resultOLabel
+            // 
+            this.resultOLabel.AutoSize = true;
+            this.resultOLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultOLabel.Location = new System.Drawing.Point(258, 507);
+            this.resultOLabel.Name = "resultOLabel";
+            this.resultOLabel.Size = new System.Drawing.Size(149, 24);
+            this.resultOLabel.TabIndex = 28;
+            this.resultOLabel.Text = "W-00; L-00; D-00";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(440, 615);
+            this.Controls.Add(this.resultOLabel);
+            this.Controls.Add(this.resultXLabel);
+            this.Controls.Add(this.nextMoveInfoLabel);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.playerOLabel);
+            this.Controls.Add(this.playerNameO_textBox);
+            this.Controls.Add(this.playerXLabel);
+            this.Controls.Add(this.playerNameX_textBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TIC-TAC-TOE";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox playerNameX_textBox;
+        private System.Windows.Forms.Label playerXLabel;
+        private System.Windows.Forms.Label playerOLabel;
+        private System.Windows.Forms.TextBox playerNameO_textBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label nextMoveInfoLabel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openLastGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveGameToolStripMenuItem;
+        private System.Windows.Forms.Label resultXLabel;
+        private System.Windows.Forms.Label resultOLabel;
+    }
+}
+
---
+++ Tic_Tac_Toe_CSHARP_FORMS/Tic_Tac_Toe_CSHARP_FORMS/MainForm.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_CSHARP_FORMS
+{
+    [Serializable]
+    public partial class MainForm : Form
+    {
+        string _nextMove;
+        int _winX, _lostX, _drawX;
+        int _winO, _lostO, _drawO;
+        string _path;
+        string _lastGameStarted;
+        public MainForm()
+        {
+            InitializeComponent();
+            _nextMove = "X";
+            playerNameX_textBox.Text = "Player 1";
+            playerNameO_textBox.Text = "Player 2";
+            _winX = _lostX = _drawX = 0;
+            _winO = _lostO = _drawO = 0;
+            _path = "default.tt";
+            _lastGameStarted = _nextMove;
+        }
+
+        private void restartGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+        private void openGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(_path))
+                {
+                    string str = sr.ReadLine();
+                    string[] tmp = str.Split(' ');
+                    if (tmp.Length != 10) throw new Exception("Wrong File");
+                    playerNameX_textBox.Text = tmp[0];
+                    playerNameO_textBox.Text = tmp[1];
+                    _nextMove = tmp[2];
+                    _winX = Int32.Parse(tmp[3]);
+                    _lostX = Int32.Parse(tmp[4]); 
+                    _drawX = Int32.Parse(tmp[5]);
+                    _winO = Int32.Parse(tmp[6]); 
+                    _lostO = Int32.Parse(tmp[7]); 
+                    _drawO = Int32.Parse(tmp[8]); ;
+                    _path = tmp[9];
+                    CheckWin();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                //Console.WriteLine(ex.Message);
+            }
+        }
+        private void saveGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //if (!File.Exists(_path)) File.Create(_path);
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(_path, false))
+                {
+                    sw.WriteLine(playerNameX_textBox.Text + " " 
+                        + playerNameO_textBox.Text + " " 
+                        + _nextMove + " " + _winX + " " + _lostX
+                        + " " + _drawX + " " + _winO + " " + _lostO
+                        + " " + _drawO + " " + _path);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        private void ClearBoard()
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+        }
+        private void NextGame()
+        {
+            if (_lastGameStarted == "X")
+            {
+                _nextMove = _lastGameStarted = "O";
+
+            }
+            else _nextMove = _lastGameStarted = "X";
+        }
+        private void RestartGame()
+        {
+            _nextMove = "X";
+            playerNameX_textBox.Text = "Player 1";
+            playerNameO_textBox.Text = "Player 2";
+            _winX = _lostX = _drawX = 0;
+            _winO = _lostO = _drawO = 0;
+            ClearBoard();
+            _nextMove = _lastGameStarted = "X";
+            CheckWin();
+        }
+        private void ChangeNextMove()
+        {
+            if (_nextMove == "X") _nextMove = "O";
+            else _nextMove = "X";
+            nextMoveInfoLabel.Text = "Next Move '" + _nextMove + "'";
+        }
+        private void CheckWin()
+        {
+            int n = CountWin();
+            if (n == 1)
+            {
+                MessageBox.Show($"Game Over!\nCongrats to {playerNameX_textBox.Text}");
+                _winX++;
+                _lostO++;
+                ClearBoard();
+                NextGame();
+            }
+            else if (n == -1)
+            {
+                MessageBox.Show($"Game Over!\nCongrats to {playerNameO_textBox.Text}");
+                _winO++;
+                _lostX++;
+                ClearBoard();
+                NextGame();
+            } 
+            else if (n == 0)
+            {
+                MessageBox.Show($"Game Over!\nDRAW!");
+                _drawX++;
+                _drawO++;
+                ClearBoard();
+                NextGame();
+            }
+            resultXLabel.Text
+                = $"W-{_winX}; L-{_lostX}; D-{_drawX}";
+            resultOLabel.Text
+                = $"W-{_winO}; L-{_lostO}; D-{_drawO}";
+        }
+        private int CountWin()
+        {
+            if ((button1.Text != "" && button1.Text == button2.Text
+                && button2.Text == button3.Text)
+                || (button4.Text != "" && button4.Text == button5.Text
+                && button5.Text == button6.Text)
+                || (button7.Text != "" && button7.Text == button8.Text
+                && button8.Text == button9.Text)
+                || (button1.Text != "" && button1.Text == button4.Text
+                && button4.Text == button7.Text)
+                || (button2.Text != "" && button2.Text == button5.Text
+                && button5.Text == button8.Text)
+                || (button3.Text != "" && button3.Text == button6.Text
+                && button6.Text == button9.Text)
+                || (button1.Text != "" && button1.Text == button5.Text
+                && button5.Text == button9.Text)
+                || (button3.Text != "" && button3.Text == button5.Text
+                && button5.Text == button7.Text))
+            {
+                if (_nextMove == "X") return 1;
+                else return -1;
+            }
+            else if (button1.Text != "" && button2.Text != ""
+                && button3.Text != "" && button4.Text != ""
+                && button5.Text != "" && button6.Text != ""
+                && button7.Text != "" && button8.Text != ""
+                && button9.Text != "") return 0;
+            return 2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text=="")
+            {
+                button1.Text = _nextMove;
+                CheckWin();
+                ChangeNextMove();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "")
+            {
+                button2.Text = _nextMove;
+                CheckWin();
+                ChangeNextMove();
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == "")
+            {
+                button3.Text = _nextMove;
+                CheckWin();
+                ChangeNextMove();
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == "")
+            {
+                button4.Text = _nextMove;
+                CheckWin();
+                ChangeNextMove();
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == "")
+            {
+                button5.Text = _nextMove;
+                CheckWin();
+                ChangeNextMove();
+            }
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Text == "")
+            {
+                button6.Text = _nextMove;
+                CheckWin();
+                ChangeNextMove();
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button7.Text == "")
+            {
+                button7.Text = _nextMove;
+                CheckWin();
+                ChangeNextMove();
+            }
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button8.Text == "")
+            {
+                button8.Text = _nextMove;
+                CheckWin();
+                ChangeNextMove();
+            }
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button9.Text == "")
+            {
+                button9.Text = _nextMove;
+                CheckWin();
+                ChangeNextMove();
+            }
+        }
+    }
+}
